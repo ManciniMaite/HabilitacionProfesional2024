e641f3f03731312617a83124d5abaaed9965fc85
Maite <maitemancini00@gmail.com>
2024-09-04 16:50:27 -0300

    inicializando back

---
+++ InitializrSpringbootProject/src/main/java/com/seminario/integrador/pawplan/PawplanApplication.java
@@ -0,0 +1,13 @@
+package com.seminario.integrador.pawplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawplanApplication.class, args);
+	}
+
+}
---
+++ InitializrSpringbootProject/src/test/java/com/seminario/integrador/pawplan/PawplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seminario.integrador.pawplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
