7fe7fdef85db85dc37709ecc2a76789d2df1365c
sebastian <sebastian@Lenovo>
2024-10-10 19:06:55 -0300

    respuesta generica que devuelven los controllers

---
+++ InitializrSpringbootProject/src/main/java/com/seminario/integrador/pawplan/controller/values/Response.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.seminario.integrador.pawplan.controller.values;
+
+/**
+ *  clase base de respuesta
+ * @author sebastian
+ */
+public class Response {
+    
+    private String estado;
+    private String mensaje;
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+    
+}
