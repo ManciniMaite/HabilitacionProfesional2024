4099aa08ecc0f72e70ca653177501bf7fd078519
ManciniMaite <maitemancini@hotmail.com>
2025-04-08 20:48:48 -0300

    guarda de acceso

---
+++ PawPanFront/src/app/access-guard/authGuard.ts
@@ -0,0 +1,21 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';; 
+import { map, take } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.usuario$.pipe(
+    take(1), // Tomamos solo el último valor
+    map(usuario => {
+      if (usuario) {
+        return true;
+      } else {
+        router.navigate(['/iniciar-sesion']);
+        return false;
+      }
+    })
+  );
+};
--- PawPanFront/src/app/app.routes.ts
+++ PawPanFront/src/app/app.routes.ts
@@ -13,24 +13,25 @@ import { CrearCuentaLocalComponent } from './crear-cuenta-local/crear-cuenta-loc
 import { AdmReservarTurnoComponent } from './adm-reservar-turno/adm-reservar-turno.component';
 import { EmergenciaComponent } from './emergencia/emergencia.component';
 import { HomeComponent } from './home/home.component';
+import { authGuard } from './access-guard/authGuard';
 
 export const routes: Routes = [
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'adm-horarios', component: AdmHorariosComponent, canActivate: [authGuard] },
+  { path: 'adm-mascotas', component: AdmMascotasComponent, canActivate: [authGuard] },
+  { path: 'adm-turnos-perdientes', component: AdmTurnosPendientesComponent, canActivate: [authGuard] },
+  { path: 'adm-turnos-reservados', component: AdmTurnosReservadosComponent, canActivate: [authGuard] },
+  { path: 'adm-reservar-turno', component: AdmReservarTurnoComponent, canActivate: [authGuard] },
+  { path: 'adm-veterinarios', component: AdmVeterinariosComponent, canActivate: [authGuard] },
+  { path: 'mascota/:id', component: MascotaEditComponent, canActivate: [authGuard] },
+  { path: 'emergencia', component: EmergenciaComponent, canActivate: [authGuard] },
 
-    { path: 'home', component: HomeComponent },
-    { path: 'adm-horarios', component: AdmHorariosComponent },
-    { path: 'adm-mascotas', component: AdmMascotasComponent },
-    { path: 'adm-turnos-perdientes', component: AdmTurnosPendientesComponent },
-    { path: 'adm-turnos-reservados', component: AdmTurnosReservadosComponent },
-    { path: 'adm-reservar-turno', component: AdmReservarTurnoComponent},
-    { path: 'adm-veterinarios', component: AdmVeterinariosComponent },
-    { path: 'crear-cuenta', component: CrearCuentaComponent },
-    { path: 'mascota/:id', component: MascotaEditComponent },
-    { path: 'iniciar-sesion', component: InicioComponent  },
-    { path: 'crear-cuenta-paciente', component: CrearCuentaClienteComponent},
-    { path: 'crear-cuenta-veterinario', component: CrearCuentaVeterinarioComponent},
-    { path: 'crear-cuenta-local', component: CrearCuentaLocalComponent},
-    { path: 'emergencia', component: EmergenciaComponent},
-    
-    { path: '**', redirectTo: 'iniciar-sesion' }
-    
+  // Rutas públicas
+  { path: 'iniciar-sesion', component: InicioComponent },
+  { path: 'crear-cuenta', component: CrearCuentaComponent },
+  { path: 'crear-cuenta-paciente', component: CrearCuentaClienteComponent },
+  { path: 'crear-cuenta-veterinario', component: CrearCuentaVeterinarioComponent },
+  { path: 'crear-cuenta-local', component: CrearCuentaLocalComponent },
+
+  { path: '**', redirectTo: 'iniciar-sesion' }
 ];
