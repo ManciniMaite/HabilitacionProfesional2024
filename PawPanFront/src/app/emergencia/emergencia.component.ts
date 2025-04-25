c08c60c4c55e8349bc1500491b15f8812f51a4a3
ManciniMaite <maitemancini@hotmail.com>
2025-04-25 09:20:47 -0300

    redireccion a mis turnos desde turno reservado - dialogo error en emergencia

--- PawPanFront/src/app/emergencia/emergencia.component.ts
+++ PawPanFront/src/app/emergencia/emergencia.component.ts
@@ -3,6 +3,9 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { Emergencia } from '../model/Emergencia';
 import { VeterinariesService } from '../services/Veterinaries-service';
+import { MatDialog } from '@angular/material/dialog';
+import { GenericDialogComponent } from '../model/dialog/generic-dialog/generic-dialog.component';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-emergencia',
@@ -31,7 +34,9 @@ export class EmergenciaComponent {
   // ];
 
   constructor(
-    private veterinarieService: VeterinariesService
+    private veterinarieService: VeterinariesService,
+    private dialog: MatDialog,
+    private location: Location
   ){}
 
   ngOnInit(){
@@ -43,9 +48,32 @@ export class EmergenciaComponent {
       next:(data) =>{
         if (data && data.estado != "ERROR"){
           this.contactos = data.contactos;
+        } else {
+          this.dialog.open(GenericDialogComponent, {
+            data: {
+              type: 'error',
+              title: '¡Algo salió mal!',
+              body: data.mensaje,
+              acceptText: 'Cerrar',
+              onAccept: () => {
+                this.location.back();
+              }
+            }
+          });
         }
       }, error: (error)=>{
         console.log(error);
+        this.dialog.open(GenericDialogComponent, {
+          data: {
+            type: 'error',
+            title: '¡Algo salió mal!',
+            body: 'Ocurrio un error al buscar los veterinarios o veterinarias disponibles',
+            acceptText: 'Cerrar',
+            onAccept: () => {
+              this.location.back();
+            }
+          }
+        });
       }
     });
   }
